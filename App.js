a38eea03bb8447bb4f9b13236a4b730d619d8719
Muhammad Usman <mug832@gmail.com>
2024-03-26 17:02:17 +0500

    okay

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import * as React from 'react';
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import LoadingScreen from "./src/screens/LoadingScreen/LoadingScreen";
-
-
 
 const Stack = createStackNavigator();
 
-function MyStack (){
+function MyStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+      {/* <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} /> */}
     </Stack.Navigator>
   );
 }
 
-export default function App(){
+
+export default function App() {
   return (
     <NavigationContainer><MyStack/></NavigationContainer>
-  )
+  );
 }
--- src/screens/LoadingScreen/LoadingScreen.js
+++ src/screens/LoadingScreen/LoadingScreen.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { StatusBar, Text, Platform, View } from "react-native";
 
 const LoadingScreen = () => {
+
+    console.log(Platform);
   return (
-    <Text>LoadingScreen</Text>
-  )
+    <>
+      <StatusBar
+        backgroundColor="#61dafb" // Sets the background color to light blue
+        barStyle="dark-content" // Sets the status bar icons and text to dark
+        animated={true} // Enables animated transitions
+        hidden={false} // Shows the status bar
+        showHideTransition="fade" // Sets the transition effect to fade
+      />
+
+      <View style={{ alignItems: "center" }}>
+        <Text>OS: {Platform.OS}</Text>
+        <Text>Version: {Platform.Version}</Text>
+      </View>
+    </>
+  );
 }
 
 export default LoadingScreen;
